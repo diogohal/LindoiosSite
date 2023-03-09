72d3351a77b72ff000ea8eca86620435c5077b91
dha21 <dha21@inf.ufpr.br>
2023-03-09 11:57:02 -0300

    changes

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.config['UPLOAD_FOLDER'] = pictureFolder
 @app.route('/', methods=['POST', 'GET'])
 def lindois():
     if request.method == 'POST':
-        # meme_generator()
+        meme_generator()
         picture = os.path.join(app.config['UPLOAD_FOLDER'], 'result.png')
         return render_template('index.html', picture=picture)
     return render_template('index.html')
