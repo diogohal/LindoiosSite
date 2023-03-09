71c7d158d370f788aa724bb5cfbeaff46480755f
dha21 <dha21@inf.ufpr.br>
2023-03-08 22:19:24 -0300

    first commit

---
+++ lindoios_site.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+from meme_generator import meme_generator
+import os
+app = Flask(__name__)
+
+pictureFolder = os.path.join('static', 'images')
+
+app.config['UPLOAD_FOLDER'] = pictureFolder
+
+@app.route('/', methods=['POST', 'GET'])
+def lindois():
+    if request.method == 'POST':
+        meme_generator()
+        picture = os.path.join(app.config['UPLOAD_FOLDER'], 'result.png')
+        return render_template('index.html', picture=picture)
+    return render_template('index.html')
+    
---
+++ meme_generator.py
@@ -0,0 +1,54 @@
+import cv2 as cv
+import numpy as np
+import random
+import os
+
+def meme_generator():
+    # Load template and figure
+    path_figure = './static/images/figures'
+    file = random.choice(os.listdir(path_figure))
+    figure = cv.imread(os.path.join(path_figure, file))
+
+    path_model = './static/images/models'
+    file_model = random.choice(os.listdir(path_model))
+    template = cv.imread(os.path.join(path_model,file_model))
+
+    figure = cv.resize(figure, (template.shape[1], template.shape[0]))
+
+    # Set blank image
+    blank = template.copy()
+    blank[:,:] = (0,0,0)
+
+    # Set the lower and upper green
+    lower_green = np.array([23,255,17])
+    upper_green = np.array([23,255,17])
+
+    # Set the masks
+    mask = cv.inRange(template, lower_green, upper_green)
+    mask_inv = cv.bitwise_not(mask)
+
+    # Set contours
+    contours, hierarchy = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    contour = np.zeros((mask.shape[0], mask.shape[1]), np.uint8)
+    cv.drawContours(contour, contours, 0, (255, 255, 255), 3)
+
+    # Find the size of the green box
+    x,y,w,h = cv.boundingRect(contour)
+    widht = w
+    height = h
+
+    # Resize the figure to box size
+    resized = cv.resize(figure, [widht, height])
+
+    # Overlay blank image
+    blank[y:y+h, x:x+w] = resized
+
+    # Apply the mask to the figure image
+    figure_masked = cv.bitwise_and(blank, blank, mask=mask)
+
+    # Apply the inverted mask to the template image
+    template_masked = cv.bitwise_and(template, template, mask=mask_inv)
+
+    # combine the masked figure and template images
+    result = cv.add(template_masked, figure_masked)
+    cv.imwrite('./static/images/result.png', result)
