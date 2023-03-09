656f7636da972e7e3436ea12954a993cd6ff7e40
dha21 <dha21@inf.ufpr.br>
2023-03-09 11:47:59 -0300

    hmmm

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from meme_generator import meme_generator
+# from meme_generator import meme_generator
 import os
 app = Flask(__name__)
 
@@ -10,7 +10,7 @@ app.config['UPLOAD_FOLDER'] = pictureFolder
 @app.route('/', methods=['POST', 'GET'])
 def lindois():
     if request.method == 'POST':
-        meme_generator()
+        # meme_generator()
         picture = os.path.join(app.config['UPLOAD_FOLDER'], 'result.png')
         return render_template('index.html', picture=picture)
     return render_template('index.html')
